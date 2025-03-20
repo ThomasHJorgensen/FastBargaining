a4d04a2a40cea125aab4b1a28ce18581173735b9
Annasofie Olesen <aso@econ.ku.dk>
2025-03-20 10:49:56 +0100

    style: clean up old precomputation code for singles

--- Application/cppfuncs/precompute.cpp
+++ Application/cppfuncs/precompute.cpp
@@ -263,39 +263,7 @@ namespace precompute{
 
     // }
 
-    // double cons_priv_single(double C_tot,int gender,par_struct *par){
-    //     // closed form solution for intra-period problem of single.
-    //     double rho = par->rho_w;
-    //     double phi = par->phi_w;
-    //     double alpha1 = par->alpha1_w;
-    //     double alpha2 = par->alpha2_w;
-    //     if (gender == man) {
-    //         rho = par->rho_m;
-    //         phi = par->phi_m;
-    //         alpha1 = par->alpha1_m;
-    //         alpha2 = par->alpha2_m;
-    //     }  
-        
-    //     return C_tot/(1.0 + pow(alpha2/alpha1,1.0/(1.0-phi) ));
-    // }
 
-
-    // void precompute_cons_interp_single(int i, int gender, par_struct* par){
-    //     double* grid_marg_u_single = par->grid_marg_u_single_w;
-    //     double* grid_marg_u_single_for_inv = par->grid_marg_u_single_w_for_inv;
-    //     if (gender==man){
-    //         grid_marg_u_single = par->grid_marg_u_single_m;
-    //         grid_marg_u_single_for_inv = par->grid_marg_u_single_m_for_inv;
-    //     }
-
-    //     // calculate marginal utility and inverse marginal utility for EGM
-    //     double delta = 0.0001;
-    //     double util_delta = utils::util_C_single(par->grid_C_for_marg_u[i] + delta,gender,par);
-    //     double util = utils::util_C_single(par->grid_C_for_marg_u[i],gender,par);
-    //     grid_marg_u_single[i] = (util_delta - util)/(delta);
-    //     grid_marg_u_single_for_inv[par->num_marg_u-1 -  i] = grid_marg_u_single[i];
-    
-    // }
 
     // void intraperiod_allocation(double* Cw_priv, double* Cm_priv, double* C_pub , double C_tot,int iP,sol_struct *sol,par_struct *par){
     //     // This function is almost identical to one used in pre-computation... to get "util_C_couple"
@@ -527,78 +495,6 @@ namespace precompute{
 
     //     delete solver_data;              
         
-    //     // return consumption value
-    //     return x[0];
-        
-    // }
-
-    // // Could be deleted, or used to compare with analytical.
-    // double obj_inv_marg_util_single(unsigned n, const double *x, double *grad, void *solver_data_in){
-    //      // unpack
-    //     solver_inv_struct *solver_data = (solver_inv_struct *) solver_data_in; 
-        
-    //     double C_tot = x[0];
-    //     double margU = solver_data->margU;
-    //     int gender = solver_data->gender;
-    //     bool do_print = solver_data->do_print;
-    //     par_struct *par = solver_data->par;
-
-    //     // clip
-    //     double penalty = 0.0;
-    //     if (C_tot <= 0.0) {
-    //         penalty += 1000.0*C_tot*C_tot;
-    //         C_tot = 1.0e-6;
-    //     }
-
-    //     // return squared difference (using analytical marginal utility)
-    //     double diff = utils::marg_util_C(C_tot, gender, par) - margU;
-
-    //     if (do_print){
-    //         logs::write("inverse_log.txt",1,"C_tot: %f, diff: %f, penalty: %f\n",C_tot,diff,penalty);
-    //     }
-    //     return diff*diff + penalty;
-
-    // }
-    
-    // double inv_marg_util_single(double margU, int gender, par_struct* par, double guess = 3.0, bool do_print=false){
-    //     // setup numerical solver
-    //     solver_inv_struct* solver_data = new solver_inv_struct;  
-                
-    //     int const dim = 1;
-    //     double lb[dim],ub[dim],x[dim];   
-        
-    //     auto opt = nlopt_create(NLOPT_LN_BOBYQA, dim); // NLOPT_LD_MMA NLOPT_LD_LBFGS NLOPT_GN_ORIG_DIRECT NLOPT_LN_BOBYQA
-    //     double minf=0.0;
-
-    //     // search over optimal total consumption, C
-    //     // settings
-    //     solver_data->margU = margU;  
-    //     solver_data->gender = gender;         
-    //     solver_data->par = par;
-    //     solver_data->do_print = do_print;        
-
-    //     if (do_print){
-    //         logs::write("inverse_log.txt",0,"margU: %f\n",margU);
-    //     }
-
-    //     nlopt_set_min_objective(opt, obj_inv_marg_util_single, solver_data);   
-    //     nlopt_set_maxeval(opt, 2000);
-    //     nlopt_set_ftol_rel(opt, 1.0e-6);
-    //     nlopt_set_xtol_rel(opt, 1.0e-5);
-
-    //     // bounds
-    //     lb[0] = 0.0;  
-    //     ub[0] = 2.0*par->max_Ctot;
-    //     nlopt_set_lower_bounds(opt, lb);
-    //     nlopt_set_upper_bounds(opt, ub);
-
-    //     // optimize
-    //     x[0] = guess; 
-    //     nlopt_optimize(opt, x, &minf);          
-    //     nlopt_destroy(opt);                 
-        
-    //     delete solver_data;
-
     //     // return consumption value
     //     return x[0];
         
